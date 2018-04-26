45fc8839eb61e2ab01b554cb7ac0bc1ce71b3de2
erichall <hallstrom.eric@gmail.com>
2018-04-26 20:28:28 +0200

    add corse

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ from flask_cors import CORS, cross_origin
 
 db_connect = create_engine('sqlite:///data.db')
 app = Flask(__name__)
-# CORS(app, resources={r"/sentiment_range":\
-#                      {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/sentiment_range":\
+                     {"origins": "http://localhost:3000"}})
 api = Api(app)
 
 
